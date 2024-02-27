e58a26a520f871e226789d1820445c2004df17fa
Alexander Curtis <145727179+alexandercurtis04@users.noreply.github.com>
2024-02-27 18:28:55 +0000

    Updated styling in index.html

--- app.js
+++ app.js
@@ -6,3 +6,20 @@ menu.addEventListener('click', function() {
     menuLinks.classList.toggle('active');
 });
 
+//side note to commit changes:
+//what I type is: Updated styling in index.html
+
+
+// Looks like this
+/*
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+#
+# On branch main
+# Your branch is up to date with 'origin/main'.
+#
+# Changes to be committed:
+#	modified:   index.html
+#
+Updated styling in index.html
+*/
